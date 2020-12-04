d3f8e317b06e395c7daa48132956602976f0c474
Zac Olds <zac.olds@gmail.com>
2020-12-04 17:09:28 -0600

    list is listing

--- src/components/CigarList.jsx
+++ src/components/CigarList.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CigarList = (props) => {
-  console.log(props);
+  console.log("props: ", props.cigar);
   if (props.cigar.id[0]) {
-    return <div>Cigar</div>;
+    return <div>{props.cigar.fields.cigar}</div>;
   } else {
     <div>Loading...</div>;
   }
