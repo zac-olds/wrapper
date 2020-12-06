920fc70ba12e702292b1054f3a36d4bc82f71d51
Zac Olds <zac.olds@gmail.com>
2020-12-06 10:27:32 -0600

    cigar detail working

--- src/components/CigarDetail.jsx
+++ src/components/CigarDetail.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { baseURL, config } from "../services";
+import { withRouter } from "react-router";
 
 class CigarDetail extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       cigar: [],
+      cigarID: props.match.params.id,
     };
   }
 
-  async getData() {
-    const resp = await axios.get(`${baseURL}/`, config);
-    // const cigarData = resp.data.records;
-    this.setState({ cigar: resp.data.records });
-  }
-
   componentDidMount() {
+    // console.log(this.match.params.id);
     this.getData();
   }
 
+  async getData() {
+    const resp = await axios.get(`${baseURL}/${this.state.cigarID}`, config);
+    console.log(resp.data.fields);
+    this.setState({ cigar: resp.data.fields });
+  }
+
   render() {
-    if (this.state.cigar[0]) {
-      return <div>{this.state.cigar[0].fields.cigar}</div>;
+    if (this.state.cigar) {
+      return <div>{this.state.cigar.cigar}</div>;
     } else {
       return <div>Loading...</div>;
     }
   }
 }
 
-export default CigarDetail;
+export default withRouter(CigarDetail);
